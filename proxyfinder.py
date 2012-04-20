52d1b8cff344c30248b47a602439b5cf8fd9f364
alexey-grom <alxistr@gmail.com>
2012-04-20 23:18:40 +0700

    9 commit

--- proxyfinder.py
+++ proxyfinder.py
@@ -2,153 +2,73 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import re
 from optparse import OptionParser
 
-from grab.spider import Spider, Task
-from grab.tools.rex import rex_text_list
+from spider import ProxyFinder
 
 
-PROXY_MASK = re.compile('(\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}([:]\d{1,5})?)')
-
-
-class ProxyFinder(Spider):
-    initial_urls = ['http://google.com']
-
-    def __init__(self,
-                 search_query='free proxy', search_count=100,
-                 fetch_urls=False, fetch_level=2,
-                 *args, **kwargs):
-        '''
-        :param search_query: Поисковой запрос для гугла
-        :param search_count: Максимальное количество резальтатов в выдаче гугла
-        :param fetch_urls: Если True - кроме основной страницы сайта
-            будет загружать остальные на глубину fetch_level
-        :param fetch_level: Глубина просмотра сайтов.
-            fetch_level=1 - это только заглавная страница
-            fetch_level=2 - заглавная страница + просмотр все url с нее. И т.д.
-        '''
-
-        super(ProxyFinder, self).__init__(*args, **kwargs)
-
-        self.search_query = search_query
-        self.search_count = search_count
-        self.fetch_urls = fetch_urls
-        self.fetch_level = fetch_level
-
-    def task_initial(self, grab, task):
-        '''
-        Инициирует поисковой запрос к гуглу
-        '''
-
-        # если не перезаписывать некоторые параметры в post -
-        # то параметр num (количество поисковых результатов) перестает работать
-
-        post = dict(
-            q = self.search_query,
-            num = self.search_count,
-            as_qdr = 'all',
-            sclient = '',
-            output = '',
-        )
-
-        grab.submit(make_request=False,
-                    extra_post=post)
-
-        yield Task(name='search',
-                   grab=grab)
-
-    def task_search(self, grab, task):
-        '''
-        Вызывается когда получен результат поискового запроса
-        '''
-
-        # выборка всех ссылок
-
-        urls = grab.xpath_list('//h3[@class="r"]/a/@href')
-
-        for url in urls:
-            # url может быть относительным, поэтому нужно скопировать grab
-            g = grab.clone()
-            grab.setup(url=url)
-            #
-            yield Task(
-                name='list',
-                grab=g,
-                level=self.fetch_level - 1
-            )
-
-    def task_list(self, grab, task):
-        '''
-        Вызывается когда получена очередная страница сайта
-        '''
-
-        # поиск всех ip[:port] на странице
-
-        proxies = rex_text_list(grab.response.unicode_body(), PROXY_MASK)
-        self.save_proxies(proxies)
-
-        # если требуется просматривать сайт - выборка всех ссылок
-        # и инициация заданий
-
-        if not self.fetch_urls or not task.level:
-            return
-
-        urls = grab.xpath_list('//a/@href')
-
-        for url in urls:
-            #
-            g = grab.clone()
-            g.setup(url=url)
-            #
-            yield Task(
-                name='list',
-                grab=g,
-                level=task.level - 1
-            )
+class Finder(ProxyFinder):
+    '''
+    Наследник поисковика с перегруженным методом
+    сохранения найденных прокси
+    '''
 
     def save_proxies(self, proxies):
-        print len(proxies)
+        '''
+        Сохраняет найденные прокси в *А КУДА?*
+        '''
+
+        # TODO: и где епта?
 
 
 def main():
     parser = OptionParser(description=u'Поисковик прокси')
 
-    parser.add_option('-t',
+    parser.add_option(
+        '-t',
         action="store",
         dest='thread_number',
         default=10,
         type='int',
-        help=u'количество потоков')
+        help=u'количество потоков'
+    )
 
-    parser.add_option('-q',
+    parser.add_option(
+        '-q',
         action="store",
         dest='search_query',
         default='free proxy',
-        help=u'поисковой запрос для гугла')
+        help=u'поисковой запрос для гугла'
+    )
 
-    parser.add_option('-c',
+    parser.add_option(
+        '-c',
         action="store",
         dest='search_count',
         default=100,
         type='int',
-        help=u'размер выдачи гугла')
+        help=u'размер выдачи гугла'
+    )
 
-    parser.add_option('-f',
+    parser.add_option(
+        '-f',
         action="store_true",
         dest='fetch_urls',
-        help=u'сканировать сайты')
+        help=u'сканировать сайты'
+    )
 
-    parser.add_option('-l',
+    parser.add_option(
+        '-l',
         action="store",
         dest='fetch_level',
         default=2,
         type='int',
-        help=u'глубина сканирования сайтов')
+        help=u'глубина сканирования сайтов'
+    )
 
     options, _ = parser.parse_args()
 
-    proxy_finder = ProxyFinder(**vars(options))
+    proxy_finder = Finder(**vars(options))
     proxy_finder.run()
     proxy_finder.render_stats()
 
---
+++ spider.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+from grab.spider import Spider, Task
+from grab.tools.rex import rex_text_list
+
+
+PROXY_MASK = re.compile('(\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}([:]\d{1,5})?)')
+
+
+class ProxyFinder(Spider):
+    '''
+    Поисковик прокси через гугл
+    '''
+
+    initial_urls = ['http://google.com']
+
+    def __init__(self,
+                 search_query='free proxy', search_count=100,
+                 fetch_urls=False, fetch_level=2,
+                 *args, **kwargs):
+        '''
+        :param search_query: Поисковой запрос для гугла
+        :param search_count: Максимальное количество резальтатов в выдаче гугла
+        :param fetch_urls: Если True - кроме основной страницы сайта
+            будет загружать остальные на глубину fetch_level
+        :param fetch_level: Глубина просмотра сайтов.
+            fetch_level=1 - это только заглавная страница
+            fetch_level=2 - заглавная страница + просмотр все url с нее. И т.д.
+        '''
+
+        super(ProxyFinder, self).__init__(*args, **kwargs)
+
+        self.search_query = search_query
+        self.search_count = search_count
+        self.fetch_urls = fetch_urls
+        self.fetch_level = fetch_level
+
+    def task_initial(self, grab, task):
+        '''
+        Инициирует поисковой запрос к гуглу
+        '''
+
+        # если не перезаписывать некоторые параметры в post -
+        # то параметр num (количество поисковых результатов) перестает работать
+
+        post = dict(
+            q = self.search_query,
+            num = self.search_count,
+            as_qdr = 'all',
+            sclient = '',
+            output = '',
+            )
+
+        grab.submit(make_request=False,
+                    extra_post=post)
+
+        yield Task(name='search',
+                   grab=grab)
+
+    def task_search(self, grab, task):
+        '''
+        Вызывается когда получен результат поискового запроса
+        '''
+
+        # выборка всех ссылок
+
+        urls = grab.xpath_list('//h3[@class="r"]/a/@href')
+
+        for url in urls:
+            # url может быть относительным, поэтому нужно скопировать grab
+            g = grab.clone()
+            grab.setup(url=url)
+            #
+            yield Task(
+                name='list',
+                grab=g,
+                level=self.fetch_level - 1
+            )
+
+    def task_list(self, grab, task):
+        '''
+        Вызывается когда получена очередная страница сайта
+        '''
+
+        # поиск всех ip[:port] на странице
+
+        proxies = rex_text_list(grab.response.unicode_body(), PROXY_MASK)
+        self.save_proxies(proxies)
+
+        # если требуется просматривать сайт - выборка всех ссылок
+        # и инициация заданий
+
+        if not self.fetch_urls or not task.level:
+            return
+
+        urls = grab.xpath_list('//a/@href')
+
+        for url in urls:
+            #
+            g = grab.clone()
+            g.setup(url=url)
+            #
+            yield Task(
+                name='list',
+                grab=g,
+                level=task.level - 1
+            )
+
+    def save_proxies(self, proxies):
+        '''
+        Вызывается когда нужно сохранить найденные прокси
+
+        :param proxies: Список найденных прокси
+        '''
