e473e72e2abdd3a7644e2464dc60cfcc4982a5f4
alexey_grom <[eqcjcb666>
2013-07-22 03:11:37 +0700

    + модели заточены под мускул, поисковик переписывается на grab

--- finder.py
+++ finder.py
@@ -1,121 +1,146 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
-from re import compile, findall
+from re import MULTILINE
 from logging import basicConfig, DEBUG, getLogger
+from urlparse import urlparse, urlsplit
+from pprint import pprint
 
-from fetcher import MultiFetcher, Task, Request
-from fetcher.utils import url_fix
-from fetcher.frontend.sqlalchemy_frontend import create_session
+from grab import Grab
+from grab.spider import Spider, Task
+from grab.tools.google import build_search_url
+from furl import furl
 
-from model import Proxy, Url
+from model import (create_session, commit_session,
+                   Site, Url, Host, Port)
 
 
 logger = getLogger('finder')
 
 
-_, session = create_session('sqlite:///proxies.sqlite')
+ip_to_int = lambda ip: reduce(lambda accumulate, x: accumulate * 256 + x, map(int, ip.split('.')))
 
 
-class ProxyFinder(MultiFetcher):
-    GOOGLE_SEARCH_QUERY = 'http proxy'
-    GOOGLE_RESULTS_COUNT = 100
+class ProxyFinder(Spider):
+    google_search_query = 'free http proxy list'
+    google_results_count = 10
     DEEP = 3
-    COUNT_FOR_DEEP_SCAN = 30
+    count_for_deep_scan = 10
 
-    PROXY_MASK = compile('(\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}[:]\d{1,5})')
+    ignore_ips = [
+        ['10.0.0.0', '10.255.255.255'],
+        ['172.16.0.0', '172.31.255.255'],
+        ['192.168.0.0 ', '192.168.255.255'],
+    ]
 
-    def on_start(self):
-        logger.info(u'Открытие гугла.')
-        yield Task(
-            url='http://google.com',
-            handler='google'
+    def __init__(self,
+                 *args, **kwargs):
+        #
+        for index, ips_range in enumerate(self.ignore_ips):
+            self.ignore_ips[index] = map(ip_to_int, ips_range)
+
+        #
+        super(ProxyFinder, self).__init__(*args, **kwargs)
+        self.setup_queue()
+
+        #
+        grab = Grab()
+        grab.setup(
+            url=build_search_url(
+                self.google_search_query,
+                per_page=self.google_results_count,
+            ),
+            user_agent='Mozilla/4.0 (compatible; MSIE 6.01; Windows NT 6.0)',
+        )
+        self.add_task(Task(
+            'search_result',
+            grab=grab
+        ))
+
+    def task_search_result(self, grab, task):
+        self.process_links(
+            grab=grab,
+            task_name='page',
+            xpath='//li[@class="g"]/h3/a/@href',
+            depp_level=0
         )
 
-    def task_google(self, task, error=None):
-        if error or task.response.status_code != 200:
-            logger.info(u'Ошибка при открытии гугла - повтор.')
-            yield task
-            return
+    def is_ignored_ip(self, ip):
+        ip = ip_to_int(ip)
+        for range_start, range_end in self.ignore_ips:
+            if range_start <= ip or ip <= range_end:
+                return True
+        return False
 
-        logger.info(u'Отправка запроса.')
+    def task_page(self, grab, task):
+        url = furl(grab.response.url)
 
-        task.get_control('q').value = ProxyFinder.GOOGLE_SEARCH_QUERY
-
-        task.submit(
-            submit_name='btnG',
-            extra_values=dict(
-                num=str(ProxyFinder.GOOGLE_RESULTS_COUNT),
-                as_qdr='all',
-                sclient='',
-                output=''
+        # поиск ip-адресов
+        ips = []
+        try:
+            ips = grab.doc.rex('(\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}([:]\d{1,5})?)', flags=MULTILINE).items
+            ips = map(
+                lambda address: address.group(0).split(':'),
+                ips
             )
-        )
-        yield task.clone(handler='result')
+            ips = filter(
+                lambda address: self.is_ignored_ip(address[0]),
+                ips
+            )
+        except:
+            pass
 
-    def task_result(self, task, error=None):
-        if error or task.response.status_code != 200:
-            logger.info(u'Ошибка при отправке запроса - повтор.')
-            yield task
+        # сохранение ip-адресов
+        for ip in ips:
+            ip = ip[:2]
+            ip[0] = ip_to_int(ip[0])
+            if len(ip) > 1:
+                Port.get_or_create(ip[0], int(ip[1]))
+            else:
+                Host.get_or_create(ip[0])
+
+        # статистика по сайтам
+        Url.get_or_create(url.hostname, url.path, found_count=len(ips))
+        commit_session()
+
+        # обход ссылок сайта в глубь, если найдены ip-адреса
+        if len(ips) < self.count_for_deep_scan:
             return
 
-        logger.info(u'Обход поисковых результатов.')
+        grab.tree.make_links_absolute(grab.response.url)
+        links = map(
+            lambda item: urlparse(unicode(item)),
+            grab.tree.xpath('//a/@href')
+        )
+        links = filter(
+            lambda link: (link.hostname == url.hostname) and not Url.is_exists(link.hostname, link.path),
+            links
+        )
+        links = set(map(
+            lambda item: item.path,
+            links
+        ))
+        if not links:
+            return
 
-        Request.connection_timeout = 5
-        Request.overall_timeout = 10
+        #pprint(links)
 
-        for url in task.html.xpath('//h3[@class="r"]/a/@href', all=True):
-            logger.info(u'Переход на %s' % url)
+        for link in links:
+            #is_exists = Url.is_exists(url.hostname, link)
+            #if is_exists:
+            #    continue
             yield task.clone(
-                handler='page',
-                url=str(url),
-                level=0,
-                priority=100
+                url=urlsplit(url.geturl(), link).geturl(),
+                level=task.level + 1 if hasattr(task, 'level') else 1,
             )
-
-    def task_page(self, task, error=None):
-        if error or task.response.status_code != 200:
-            return
-
-        task.html.make_links_absolute()
-
-        ips = set(
-            ip
-            for ip in findall(self.PROXY_MASK, task.response.content)
-        )
-
-        if not ips:
-            logger.info(u'Не найдены ip-адреса на %s!' % task.response.url)
-
-        else:
-            logger.info(u'Найдено %d ip-адресов на %s.' % (len(ips), task.response.url))
-
-            for ip in ips:
-                Proxy.store_proxy(session, ip)
-
-            if task.level < ProxyFinder.DEEP and len(ips) > ProxyFinder.COUNT_FOR_DEEP_SCAN:
-                urls = set(
-                    url_fix(unicode(url))
-                    for url in task.html.xpath('//a/@href', all=True)
-                )
-                for url in urls:
-                    if not Url.is_exists(session, url):
-                        yield Task(
-                            handler='page',
-                            url=url,
-                            level=task.level + 1,
-                            priority=task.priority - 1
-                        )
-
-            session.commit()
 
 
 if __name__ == '__main__':
     basicConfig(level=DEBUG)
 
-    proxy_finder = ProxyFinder(
-        threads_count=30
-    )
-    proxy_finder.start()
-    proxy_finder.render_stat()
+    create_session('root', '654321', echo=False)
 
-    print u'Найдено %d ip-адресов' % Proxy.count(session)
+    proxy_finder = ProxyFinder(
+        thread_number=1,
+        debug_error=False,
+    )
+    proxy_finder.run()
--- model.py
+++ model.py
@@ -1,72 +1,180 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
-from datetime import datetime
+from logging import getLogger
 
-from fetcher.frontend.sqlalchemy_frontend import Model
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, or_
+from sqlalchemy import create_engine
+from sqlalchemy import (Column,
+                        Integer, SmallInteger, String, TIMESTAMP,
+                        text,
+                        ForeignKey)
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.sql.expression import exists
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, backref
 
 
-class Proxy(Model):
-    __tablename__ = 'proxies'
+logger = getLogger('storage')
+
+
+Base = declarative_base()
+Session = sessionmaker()
+session = None
+
+
+def create_session(user, password,
+                   database='proxy_finder',
+                   host='localhost', port=3306,
+                   engine='mysql',
+                   echo=False):
+    global session
+    engine = create_engine(
+        '%(engine)s://%(user)s:%(password)s@%(host)s:%(port)d/%(database)s?charset=utf8' % dict(
+            user=user,
+            password=password,
+            database=database,
+            host=host,
+            port=port,
+            engine=engine
+        ),
+        echo=echo
+    )
+    Base.metadata.create_all(engine)
+    Session.configure(bind=engine)
+    session = Session()
+
+
+def commit_session():
+    session.commit()
+
+
+class Site(Base):
+    __tablename__ = 'sites'
 
     id = Column(Integer, primary_key=True)
+    domain = Column(String(256))
+    #urls
 
-    ip = Column(String(21), index=True, unique=True)
-    check_time = Column(DateTime)
-    valid = Column(Boolean)
-
-    @classmethod
-    def store_proxy(cls, session, ip):
-        if not session.query(Proxy).filter_by(ip=ip).first():
-            session.add(Proxy(ip=ip))
-
-    @classmethod
-    def store_result(cls, session, ip, is_good=True):
-        item = session.query(Proxy).filter_by(ip=ip).first()
-        item.valid = is_good
-        item.check_time = datetime.now()
-        session.commit()
-
-    @classmethod
-    def count(cls, session):
-        return session.query(Proxy).count()
-
-    @classmethod
-    def valid_count(cls, session):
-        return session.query(Proxy).filter_by(valid=True).count()
-
-    @classmethod
-    def iterator(cls, session, count=30):
-        query = session.query(Proxy).filter_by(check_time=None)
-        table_size = query.count()
-
-        for offset in xrange(0, table_size, count):
-            for proxy in query.offset(offset).limit(count).all():
-                yield proxy
-
-    @classmethod
-    def valid_iterator(cls, session, count=30):
-        query = session.query(Proxy).filter_by(valid=True)
-        table_size = query.count()
-
-        for offset in xrange(0, table_size, count):
-            for proxy in query.offset(offset).limit(count).all():
-                yield proxy
+    @staticmethod
+    def get_or_create(domain):
+        result = session.query(Site).\
+            filter_by(domain=domain).\
+            first()
+        if not result:
+            result = Site(domain=domain)
+            session.add(result)
+        return result
 
 
-class Url(Model):
+class Url(Base):
     __tablename__ = 'urls'
 
     id = Column(Integer, primary_key=True)
-    url = Column(String(255), index=True, unique=True)
+    domain_id = Column(Integer, ForeignKey('sites.id'), nullable=False)
+    domain = relationship('Site', backref=backref('urls'))
+    path = Column(String(1024))
+    found_count = Column(Integer, default=0)
+    check_date = Column(TIMESTAMP, server_default=text('CURRENT_TIMESTAMP'), server_onupdate=text('CURRENT_TIMESTAMP'))
 
-    @classmethod
-    def is_exists(cls, session, url):
-        if not session.query(Url).filter_by(url=url).first():
-            session.add(Url(url=url))
-            return False
-        return True
+    @staticmethod
+    def get_or_create(domain, path, **kwargs):
+        url = session.query(Url).join(Site).\
+            filter(Site.domain == domain, Url.path == path).\
+            first()
+        if not url:
+            url = Url(path=path, **kwargs)
+            domain = Site.get_or_create(domain)
+            domain.urls.append(url)
+            session.merge(domain)
+        return url
 
-    @classmethod
-    def delete_all(cls, session):
-        session.query(Url).delete()
+    @staticmethod
+    def is_exists(domain, path):
+        # TODO: refactor for exists
+        result = session.query(Url).join(Site).\
+            filter(Site.domain == domain, Url.path == path).\
+            first()
+        return result is not None
+
+
+class Host(Base):
+    __tablename__ = 'hosts'
+
+    id = Column(Integer, primary_key=True)
+    ip = Column(INTEGER(unsigned=True), unique=True)
+    #ports
+
+    @staticmethod
+    def get_or_create(ip):
+        if not isinstance(ip, int):
+            raise TypeError()
+        result = session.query(Host).\
+            filter_by(ip=ip).\
+            first()
+        if not result:
+            result = Host(ip=ip)
+            session.add(result)
+        return result
+
+    def __repr__(self):
+        return '<Host %s>' % self.ip
+
+
+class Port(Base):
+    __tablename__ = 'ports'
+
+    id = Column(Integer, primary_key=True)
+    ip_id = Column(Integer, ForeignKey('hosts.id'), nullable=False)
+    ip = relationship('Host', backref=backref('ports'))
+    port = Column(SmallInteger, nullable=False)
+    check_date = Column(TIMESTAMP, server_onupdate=text('CURRENT_TIMESTAMP'))
+
+    @staticmethod
+    def get_or_create(ip, port):
+        if not isinstance(ip, int) or not isinstance(port, int):
+            raise TypeError()
+        result = session.query(Port).join(Host).\
+            filter(Host.ip == ip, Port.port == port).\
+            first()
+        if not result:
+            result = Port(port=port)
+            host = Host.get_or_create(ip)
+            host.ports.append(result)
+            session.merge(host)
+        return result
+
+    def __repr__(self):
+        return '<Port %s:%s>' % (self.ip, self.port)
+
+
+if __name__ == '__main__':
+    create_session('root', '654321', echo=False)
+
+    domain = 'google.com'
+    urls = [
+        '/sfsfsf',
+        '/sfsfsf',
+        '/sfsfsf2',
+        '/sfsfsf3',
+        '/sfsfsf4',
+    ]
+
+    ip_to_int = lambda ip: reduce(lambda accumulate, x: accumulate * 256 + x, map(int, ip.split('.')))
+    ips = [
+        ('1.1.1.1', [80, 22]),
+        ('1.1.2.1', [80, 22]),
+        ('1.1.2.1', [8080, 2222, 80]),
+        ('1.1.3.1', [80, 22]),
+    ]
+
+    print Site.get_or_create(domain)
+    for url in urls:
+        print Url.get_or_create(domain, url)
+
+    for ip, ports in ips:
+        ip = ip_to_int(ip)
+        print Host.get_or_create(ip)
+        for port in ports:
+            print Port.get_or_create(ip, port)
+
+    session.commit()
