0fcd2bd74a6c563fd0c77df2a96c85b959dff645
alexey-grom <alxgrmv@gmail.com>
2013-12-30 05:20:33 +0700

    + about

--- proxyfinder/urls.py
+++ proxyfinder/urls.py
@@ -1,6 +1,5 @@
-from datetime import timedelta
-
 from django.conf.urls import url, patterns, include
+from django.views.generic import TemplateView
 
 from views import ProxiesListView
 
@@ -9,5 +8,8 @@ from views import ProxiesListView
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
     #url(r'^api/', include(router.urls)),
-    url(r'^$', ProxiesListView.as_view(), name='proxylist'),
+    url(r'^$',
+        TemplateView.as_view(template_name='proxyfinder/about.html'),
+        name='about'),
+    url(r'^proxylist$', ProxiesListView.as_view(), name='proxylist'),
 )
